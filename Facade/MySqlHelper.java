7f8044daa2ff61c8ad24637b687bec34e6e09cb6
Kavisha Chiranjaya Abeynayaka <87388691+kaviabey@users.noreply.github.com>
2022-09-21 22:56:28 +0530

    hehe5

---
+++ Facade/MySqlHelper.java
@@ -0,0 +1,16 @@
+package Facade;
+import java.sql.Connection;
+public class MySqlHelper {
+    public static Connection getMySqlDBConnection() {         //get MySql DB connection using connection parameters         return null;
+        //get MySql DB connection using connection paramters
+        return null;
+
+    }
+
+    public void generateMySqlPDFReport(String tableName, Connection con){             };
+    public void generateMySqlHTMLReport(String tableName, Connection con){       } ;
+
+}
+
+
+
---
+++ Facade/OracleHelper.java
@@ -0,0 +1,21 @@
+package Facade;
+import java.sql.Connection;
+public class OracleHelper {
+
+    public static Connection getOracleDBConnection()
+    {
+        return null;
+    }
+
+
+    public void generateOraclePDFReport(String tableName,Connection con)
+    {
+        //get data from table and genereate pdf reprot
+    }
+
+    public void generateOracleHTMLReport(String tableName,Connection con)
+    {
+        //get data from table and generate pdf reprot
+    }
+
+}
